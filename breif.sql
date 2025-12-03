e616e70ef72f29c184aac300b6832ff35c38da10
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-12-03 15:15:04 +0100

    create article table

--- breif.sql
+++ breif.sql
@@ -12,3 +12,16 @@ create table category (
     categoryId int auto_increment primary key,
     name varchar(50) not null
 );
+
+create table article (
+    articleId int AUTO_INCREMENT primary key,
+    title varchar(100) not null,
+    content text not null,
+    creation_date date not null,
+    modification_date date not null,
+    categoryId int,
+    username varchar(50),
+
+    foreign key (categoryId) references category(categoryId),
+    foreign key (username) references user(username)
+);
